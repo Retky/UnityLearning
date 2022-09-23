a670bb331d4683b999b4e539942ce4eb5feda1b3
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-09-23 14:29:33 -0500

    Add 'player jump'
    Use gravity Modifier & force

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     // Initialize variables
     private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         // Get the rigidbody component
         playerRb = GetComponent<Rigidbody>();
+        // Set the gravity modifier
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 }
