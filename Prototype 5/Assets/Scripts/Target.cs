1d87fe547285595c889128637fb34b77b7b9e01c
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-09-26 18:07:29 -0500

    Add 'score' rules
    + Add score
    + Remove score when click a bad object
    + Remove score if object falls out

--- Prototype 5/Assets/Scripts/Target.cs
+++ Prototype 5/Assets/Scripts/Target.cs
@@ -11,10 +11,12 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -2;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
@@ -39,10 +41,21 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.UpdateScore(10);
+        } else if (gameObject.CompareTag("Bad"))
+        {
+            gameManager.UpdateScore(-25);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.UpdateScore(-5);
+        }
     }
 }
