12fa53b7fb906f2221c316e5e973794fbc0a9f61
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-09-28 18:53:54 -0500

    Add 'TitleMenu' with buttons
    +Multiplayer
    + Play
    + Quit

--- Prototype 1/Assets/Scripts/GameManager.cs
+++ Prototype 1/Assets/Scripts/GameManager.cs
@@ -7,7 +7,8 @@ public class GameManager : MonoBehaviour
     // Initialize variables
     public bool isGameActive = false;
     public GameObject titleScreen;
-    public GameObject gameOverScreen;
+    public GameObject player1;
+    public GameObject player2;
 
     public void StartGame()
     {
@@ -15,5 +16,38 @@ public class GameManager : MonoBehaviour
         isGameActive = true;
         // Hide the title screen
         titleScreen.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        // Quit the game
+        Application.Quit();
+    }
+
+    public void Multiplayer()
+    {
+        // Enable the second player
+        player2.SetActive(true);
+
+        // Change player 1 camera size
+        player1.transform.Find("Main Camera1").GetComponent<Camera>().rect = new Rect(0, 0, 0.5f, 1);
+        player1.transform.Find("Second Camera1").GetComponent<Camera>().rect = new Rect(0, 0, 0.5f, 1);
+
+        // Move player 1 to the left
+        player1.transform.position = new Vector3(-5, 0, 0);
+    }
+
+    public void Singleplayer()
+    {
+        // Enable the second player
+        player2.SetActive(false);
+
+        // Change player 1 camera size
+        player1.transform.Find("Main Camera1").GetComponent<Camera>().rect = new Rect(0, 0, 1, 1);
+        player1.transform.Find("Second Camera1").GetComponent<Camera>().rect = new Rect(0, 0, 1, 1);
+
+        // Move player 1 to the left
+        player1.transform.position = new Vector3(0, 0, 0);
     }
 }
