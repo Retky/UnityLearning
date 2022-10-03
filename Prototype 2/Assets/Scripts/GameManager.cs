fe0923b051474b467818f3781c12a6735463dd7a
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-10-02 19:51:28 -0500

    Modify 'GameMangaer' syntax

--- Prototype 2/Assets/Scripts/GameManager.cs
+++ Prototype 2/Assets/Scripts/GameManager.cs
@@ -5,22 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    bool gameHasEnded = false;
+    public bool isGameActive = true;
 
     // EndGame is a public function that can be called from other scripts
     public void EndGame()
     {
-        if (gameHasEnded == false)
+        if (isGameActive == true)
         {
-            gameHasEnded = true;
-            Debug.Log("Game Over");
+            isGameActive = false;
 
             Invoke("Restart", 4f);
         }
     }
 
     // Restart the game
-    void Restart()
+    public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
