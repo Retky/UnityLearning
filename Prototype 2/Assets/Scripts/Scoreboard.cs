c9a0b3af39396301c9906a143dfa7aefb291ed60
J. Luis Arredondo <jose.arredondo.a@outlook.com>
2022-10-10 19:17:11 -0500

    Modify 'Scoreboard' to use a List of scores

--- Prototype 2/Assets/Scripts/Scoreboard.cs
+++ Prototype 2/Assets/Scripts/Scoreboard.cs
@@ -7,6 +7,7 @@ public class Scoreboard : MonoBehaviour
 {
   private Transform entryContainer;
   private Transform entryTemplate;
+  private List<ScoreEntry> scoreEntryList = new List<ScoreEntry>();
 
   private void Awake()
   {
@@ -17,42 +18,42 @@ public class Scoreboard : MonoBehaviour
 
     float templateHeight = 40f;
 
-    for (int i = 0; i < 10; i++)
+    AddScoreEntry(1000, "AAA");
+    AddScoreEntry(900, "BBB");
+    AddScoreEntry(400, "CCC");
+    AddScoreEntry(1200, "DDD");
+    AddScoreEntry(500, "EEE");
+    AddScoreEntry(700, "FFF");
+    AddScoreEntry(800, "GGG");
+    AddScoreEntry(900, "HHH");
+    AddScoreEntry(1000, "III");
+    AddScoreEntry(1100, "JJJ");
+
+    scoreEntryList.Sort((a, b) => b.score.CompareTo(a.score));
+
+    scoreEntryList.ForEach(scoreEntry =>
     {
       Transform entryTransform = Instantiate(entryTemplate, entryContainer);
       RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-      entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+      entryRectTransform.anchoredPosition = new Vector2(0, -(templateHeight * scoreEntryList.IndexOf(scoreEntry)));
       entryTransform.gameObject.SetActive(true);
 
-      int rank = i + 1;
-      string rankString;
-      switch (rank)
-      {
-        default:
-          rankString = rank + "TH";
-          break;
-        case 1:
-          rankString = "1ST";
-          break;
-        case 2:
-          rankString = "2ND";
-          break;
-        case 3:
-          rankString = "3RD";
-          break;
-      }
-
-      entryTransform.Find("Rank").GetComponent<TextMeshProUGUI>().text = rankString;
-
-      int score = Random.Range(0, 100);
-      entryTransform.Find("Score").GetComponent<TextMeshProUGUI>().text = score.ToString();
-
-      string name = "AAA";
-      entryTransform.Find("Name").GetComponent<TextMeshProUGUI>().text = name;
-    }
+      entryTransform.Find("Rank").GetComponent<TextMeshProUGUI>().text = (scoreEntryList.IndexOf(scoreEntry) + 1).ToString();
+      entryTransform.Find("Score").GetComponent<TextMeshProUGUI>().text = scoreEntry.score.ToString();
+      entryTransform.Find("Name").GetComponent<TextMeshProUGUI>().text = scoreEntry.name;
+    });
   }
 
-  private class ScoreEntry
+  public void AddScoreEntry(int score, string name)
+  {
+    // Create ScoreEntry
+    ScoreEntry scoreElement = new ScoreEntry { score = score, name = name };
+
+    // Add to list
+    scoreEntryList.Add(scoreElement);
+  }
+
+  class ScoreEntry
   {
     public int score;
     public string name;
